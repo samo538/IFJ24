7f92042f9f60ce9f06511c4eaa03785bacf4690f
Samuel Luptak <xluptas00@stud.fit.vutbr.cz>
2024-10-21 21:43:30 +0200

    function headers done

--- syntax/syntax.c
+++ syntax/syntax.c
@@ -14,16 +14,18 @@
 #include <stdio.h>
 
 void dealloc_stoken(TokenStoragePtr stoken){
-    if (stoken->SToken != NULL){
-        dealloc_token(stoken->SToken);
-        stoken->SToken = NULL;
+    if (stoken != NULL){
+        if (stoken->SToken != NULL){
+            dealloc_token(stoken->SToken);
+            stoken->SToken = NULL;
+        }
+        if (stoken->SStoredToken != NULL){
+            dealloc_token(stoken->SStoredToken);
+            stoken->SStoredToken = NULL;
+        }
+        free(stoken);
+        stoken = NULL;
     }
-    if (stoken->SStoredToken != NULL){
-        dealloc_token(stoken->SStoredToken);
-        stoken->SStoredToken = NULL;
-    }
-    free(stoken);
-    stoken = NULL;
 }
 
 void syn_error(TokenStoragePtr stoken){
@@ -38,6 +40,7 @@ bool t_semicolon(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -48,6 +51,7 @@ bool t_cl_bracket(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -58,6 +62,51 @@ bool t_op_bracket(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_op_sq_bracket(TokenStoragePtr stoken){
+    if (stoken->SToken->type == OPENING_SQUARE_BRACKET){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_cl_sq_bracket(TokenStoragePtr stoken){
+    if (stoken->SToken->type == CLOSING_SQUARE_BRACKET){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_op_cr_bracket(TokenStoragePtr stoken){
+    if (stoken->SToken->type == OPENING_CURLY_BRACKET){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_cl_cr_bracket(TokenStoragePtr stoken){
+    if (stoken->SToken->type == CLOSING_CURLY_BRACKET){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -68,6 +117,7 @@ bool string_prolog(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -78,6 +128,7 @@ bool t_at_import(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -88,6 +139,7 @@ bool t_eq(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -98,6 +150,7 @@ bool t_ifj(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
     }
 }
@@ -108,14 +161,179 @@ bool t_const(TokenStoragePtr stoken){
         return true;
     }
     else{
+        syn_error(stoken);
         return false;
+    }
+}
+
+bool t_eof(TokenStoragePtr stoken){
+    if (stoken->SToken->type == END_OF_FILE){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_pub(TokenStoragePtr stoken){
+    if (stoken->SToken->type == PUB){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_fn(TokenStoragePtr stoken){
+    if (stoken->SToken->type == FN){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_id(TokenStoragePtr stoken){
+    if (stoken->SToken->type == ID){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_comma(TokenStoragePtr stoken){
+    if (stoken->SToken->type == COMMA){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+bool t_colon(TokenStoragePtr stoken){
+    if (stoken->SToken->type == COLON){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+
+bool t_u8(TokenStoragePtr stoken){
+    if (stoken->SToken->type == U8){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+
+bool t_type_keyword(TokenStoragePtr stoken){
+    if (stoken->SToken->type == OPENING_SQUARE_BRACKET){
+        return t_op_sq_bracket(stoken) &&
+        t_cl_sq_bracket(stoken) &&
+        t_u8(stoken);
+    }
+    else if (stoken->SToken->type == F64){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else if (stoken->SToken->type == I32){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        syn_error(stoken);
+        return false;
+    }
+}
+
+bool t_nullable(TokenStoragePtr stoken){
+    if (stoken->SToken->type == NULLABLE){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else{
+        return true;
+    }
+}
+
+bool t_type(TokenStoragePtr stoken){
+    return t_nullable(stoken) &&
+    t_type_keyword(stoken);
+}
+
+bool t_type_fndef(TokenStoragePtr stoken){
+    if (stoken->SToken->type == VOID){
+        dealloc_token(stoken->SToken);
+        stoken->SToken = next_token();
+        return true;
+    }
+    else {
+        return t_type(stoken);
     }
 }
 
 //--------------------------------------------------------
 
-bool functions(){
+bool params(TokenStoragePtr stoken){
+    if (stoken->SToken->type == ID){
+        return t_id(stoken) &&
+        t_colon(stoken) &&
+        t_type(stoken) &&
+        t_comma(stoken) &&
+        params(stoken);
+    }
+    else {
+        return true;
+    }
+}
+
+bool fn_def(TokenStoragePtr stoken){
+    return t_pub(stoken) &&
+    t_fn(stoken) &&
+    t_id(stoken) &&
+    t_op_bracket(stoken) &&
+    params(stoken) &&
+    t_cl_bracket(stoken) &&
+    t_type_fndef(stoken);
+}
+
+bool fn_body(TokenStoragePtr stoken){
     return true;
+}
+
+bool functions(TokenStoragePtr stoken){
+    if (stoken->SToken->type == END_OF_FILE) {
+        return t_eof(stoken);
+    }
+    else {
+        return fn_def(stoken) &&
+        t_op_cr_bracket(stoken) &&
+        fn_body(stoken) &&
+        t_cl_cr_bracket(stoken) &&
+        functions(stoken);
+    }
 }
 
 bool import_def(TokenStoragePtr stoken){
@@ -130,7 +348,7 @@ bool import_def(TokenStoragePtr stoken){
 }
 
 bool start(TokenStoragePtr stoken){
-    return import_def(stoken) && functions();
+    return import_def(stoken) && functions(stoken);
 }
 
 int main(){
