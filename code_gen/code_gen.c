57b6619da484b716c0bdd8528e7fe41b132cd0eb
veveLP <lukasekhouzar@seznam.cz>
2024-11-08 13:44:11 +0100

    inicial branch commit

---
+++ code_gen/code_gen.c
@@ -0,0 +1,27 @@
+//
+// Created by veve on 8.11.24.
+//
+#include "code_gen.h"
+
+void generate_code(TreeElementPtr tree) {
+    //tree->
+    printf(".IFJcode24\n");
+
+}
+
+void generate_function(TreeElementPtr tree) {
+
+}
+
+void generate_expression(TreeElementPtr tree) {
+
+}
+
+void generate_func_call(TreeElementPtr tree) {
+
+}
+
+void generate_condition(TreeElementPtr treeIf,TreeElementPtr treeElse) {
+
+}
+
