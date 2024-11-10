56ba411cea9760577fb4aeda3e37a4c79c1ebe23
Samuel Luptak <xluptas00@stud.fit.vutbr.cz>
2024-11-10 16:37:43 +0100

    as

--- tree/tree.h
+++ tree/tree.h
@@ -5,12 +5,33 @@
 
 #include "../lexer/token.h"
 
+enum NodeTypes{
+    // Top nodes
+    ROOT_NODE,
+    TOP_FUNCTION_NODE,
+    // Main sub nodes
+    ASSIGN_NODE,
+    DEFINITION_NODE,
+    RETURN_NODE,
+    WHILE_NODE,
+    IF_NODE,
+    FUNCTION_NODE,
+    IFJ_FUNCTION_NODE,
+    // "Leaf" nodes
+    EXPRESSION_NODE,
+    ID_NODE,
+};
+
 typedef struct TreeData
 {
     TokenPtr Token;
+    enum NodeTypes NodeType;
+
+    // Used by precedence
     int Type;
     bool ChangeType;
-
+    // Used by code gen
+    bool isNullCond;
 }TreeData;
 
 typedef struct TreeElement
