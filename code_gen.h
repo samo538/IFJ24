82665eff26e15313476b15e3d144c5e007e9d7ba
veveLP <lukasekhouzar@seznam.cz>
2024-11-26 15:34:50 +0100

    add coments to functions in code_gen.h

--- code_gen.h
+++ code_gen.h
@@ -67,20 +67,65 @@ void gen_expression(TreeElementPtr tree);
  * generates condition from tree
  */
 void gen_condition(TreeElementPtr tree, bool isMain);
+/*
+ * generates ifj.write function
+ */
 void gen_ifj_write(TreeElementPtr tree);
+/*
+ * generates ifj.leght function
+ */
 void gen_ifj_length(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.string function
+ */
 void gen_ifj_string(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.concat function
+ */
 void gen_ifj_concat(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.readstr function
+ */
 void gen_ifj_readstr(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.readi32 function
+ */
 void gen_ifj_readi32(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.readf64 function
+ */
 void gen_ifj_readf64(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.f2i function
+ */
 void gen_ifj_f2i(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.i2f function
+ */
 void gen_ifj_i2f(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.chr function
+ */
 void gen_ifj_chr(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.ord function
+ */
 void gen_ifj_ord(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.strcmp function
+ */
 void gen_ifj_strcmp(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * generates ifj.substring function
+ */
 void gen_ifj_substring(TreeElementPtr tree, TreeElementPtr var);
+/*
+ * returns pointer to variable name from tree
+ */
 char* get_var_name(TreeElementPtr tree);
+/*
+ * returns pointer to variable name, same function as get_var_name, but the argument is Elem_id* instead of TreeElementPtr and does not get var name for _
+ */
 char* get_var_name_from_table(Elem_id* TableElement);
 /*
  * puts every definition (DEFVAR) before start of a while, otherwise there would be multiple DEFVAR for a single variable, argument is element of while, changes definition nodes to assign nodes
